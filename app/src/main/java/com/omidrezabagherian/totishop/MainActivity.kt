0943804b1d9fd0f19ea96a427f9522c8318e4ad3
Omidreza Bagherian <OmidCoder21@gmail.com>
2022-05-29 01:15:01 -0700

    Add bottom navigation and config theme application and add menu

--- app/src/main/java/com/omidrezabagherian/totishop/MainActivity.kt
+++ app/src/main/java/com/omidrezabagherian/totishop/MainActivity.kt
@@ -2,6 +2,7 @@ package com.omidrezabagherian.totishop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.omidrezabagherian.totishop.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +14,24 @@ class MainActivity : AppCompatActivity() {
 
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
 
+        mainBinding.bottomNavigationViewMain.setOnItemSelectedListener { tab ->
+            when (tab.itemId) {
+                R.id.houseTab -> {
+                    Toast.makeText(applicationContext,R.string.text_tab_house,Toast.LENGTH_SHORT).show()
+                }
+                R.id.categoryTab -> {
+                    Toast.makeText(applicationContext,R.string.text_tab_category,Toast.LENGTH_SHORT).show()
+                }
+                R.id.bagTab -> {
+                    Toast.makeText(applicationContext,R.string.text_tab_bag,Toast.LENGTH_SHORT).show()
+                }
+                R.id.userTab -> {
+                    Toast.makeText(applicationContext,R.string.text_tab_user,Toast.LENGTH_SHORT).show()
+                }
+            }
 
+            true
+        }
 
         setContentView(mainBinding.root)
     }
