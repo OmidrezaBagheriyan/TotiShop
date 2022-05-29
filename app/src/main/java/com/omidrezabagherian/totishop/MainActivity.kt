e1a2cdebdbc0b7a32daa21cb3a54942f9c697bd0
Omidreza Bagherian <OmidCoder21@gmail.com>
2022-05-29 00:00:37 -0700

    Add view binding and remove action bar

--- app/src/main/java/com/omidrezabagherian/totishop/MainActivity.kt
+++ app/src/main/java/com/omidrezabagherian/totishop/MainActivity.kt
@@ -2,10 +2,19 @@ package com.omidrezabagherian.totishop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.omidrezabagherian.totishop.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+
+
+        setContentView(mainBinding.root)
     }
 }
