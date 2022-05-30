489f35765bd4f0380552a5eb46022b26b6b8f112
Omidreza Bagherian <OmidCoder21@gmail.com>
2022-05-30 06:25:12 -0700

    Add retrofit module in application

---
+++ app/src/main/java/com/omidrezabagherian/totishop/di/RetrofitModule.kt
@@ -0,0 +1,64 @@
+package com.omidrezabagherian.totishop.di
+
+import com.omidrezabagherian.totishop.data.remote.ShopService
+import com.omidrezabagherian.totishop.util.Values
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+
+    @Singleton
+    @Provides
+    fun provideJsonConvertorFactory(): GsonConverterFactory = GsonConverterFactory.create()
+
+    @Singleton
+    @Provides
+    fun provideHttpLogging(): HttpLoggingInterceptor {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        return httpLoggingInterceptor
+    }
+
+    @Singleton
+    @Provides
+    fun provideClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(
+                Interceptor { chain ->
+                    val request = chain.request().newBuilder().build()
+                    val response = chain.proceed(request)
+                    response
+                }
+            )
+            .addNetworkInterceptor(httpLoggingInterceptor)
+            .callTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        gsonConverterFactory: GsonConverterFactory,
+        okHttpClient: OkHttpClient
+    ): Retrofit = Retrofit.Builder()
+        .baseUrl(Values.BASE_URL)
+        .addConverterFactory(gsonConverterFactory)
+        .client(okHttpClient)
+        .build()
+
+    @Singleton
+    @Provides
+    fun provideService(retrofit: Retrofit): ShopService = retrofit.create(ShopService::class.java)
+
+}
---
+++ app/src/main/java/com/omidrezabagherian/totishop/util/Values.kt
@@ -0,0 +1,5 @@
+package com.omidrezabagherian.totishop.util
+
+object Values {
+    const val BASE_URL = "https://woocommerce.maktabsharif.ir/wp-json/wc/v3"
+}
