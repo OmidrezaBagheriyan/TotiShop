7ba24bd57772984b4e61b1165e91d461b2ba980a
Omidreza Bagherian <OmidCoder21@gmail.com>
2022-05-31 03:04:08 -0700

    Get data in server and fix bugs and config house xml

--- app/src/main/java/com/omidrezabagherian/totishop/ui/house/HouseFragment.kt
+++ app/src/main/java/com/omidrezabagherian/totishop/ui/house/HouseFragment.kt
@@ -2,27 +2,50 @@ package com.omidrezabagherian.totishop.ui.house
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.omidrezabagherian.totishop.R
+import com.omidrezabagherian.totishop.databinding.FragmentHouseBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
 @AndroidEntryPoint
 class HouseFragment : Fragment(R.layout.fragment_house) {
 
-    private val houseViewModel:HouseViewModel by viewModels()
+    private lateinit var houseBinding: FragmentHouseBinding
+    private val houseViewModel: HouseViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        val productsMap = hashMapOf<String,String>().apply {
-            put("products","popularity")
+        houseBinding = FragmentHouseBinding.bind(view)
+
+        val productsMap1 = HashMap<String, String>().apply {
+            put("orderby", "date")
         }
-        houseViewModel.getProductList(productsMap)
+        val productsMap2 = HashMap<String, String>().apply {
+            put("orderby", "popularity")
+        }
+        val productsMap3 = HashMap<String, String>().apply {
+            put("orderby", "rating")
+        }
 
+        houseViewModel.getProductList(productsMap1)
 
-        Log.i("Log1",houseViewModel.productList.value.toString())
-        Log.i("Log2",houseViewModel.productError.value.toString())
+        houseViewModel.productList.observe(viewLifecycleOwner) {
+            Log.i("Log1", it.toString())
+        }
+
+        houseViewModel.getProductList(productsMap2)
+        houseViewModel.productList.observe(viewLifecycleOwner) {
+            Log.i("Log2", it.toString())
+        }
+
+        houseViewModel.getProductList(productsMap3)
+        houseViewModel.productList.observe(viewLifecycleOwner) {
+            Log.i("Log3", it.toString())
+        }
 
     }
 }
--- app/src/main/java/com/omidrezabagherian/totishop/ui/house/HouseViewModel.kt
+++ app/src/main/java/com/omidrezabagherian/totishop/ui/house/HouseViewModel.kt
@@ -1,32 +1,40 @@
 package com.omidrezabagherian.totishop.ui.house
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.omidrezabagherian.totishop.data.ShopRepository
 import com.omidrezabagherian.totishop.data.remote.ShopService
 import com.omidrezabagherian.totishop.domain.model.Product
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
 class HouseViewModel @Inject constructor(private val shopRepository: ShopRepository) : ViewModel() {
 
-    private val _productList = MutableStateFlow<List<Product>>(emptyList())
-    val productList = _productList.asStateFlow()
+    private val _productList = MutableLiveData<List<Product>>()
+    val productList: LiveData<List<Product>> = _productList
 
-    private val _productError = MutableStateFlow(false)
-    val productError = _productError.asStateFlow()
+    private val _productError = MutableLiveData(false)
+    val productError: LiveData<Boolean> = _productError
 
     fun getProductList(filter: Map<String, String>) {
         viewModelScope.launch {
             val responseProductList = shopRepository.getProductList(1, 1, filter)
-            if (responseProductList.isSuccessful) {
-                _productList.value = responseProductList.body()!!
-            } else {
-                _productError.value = true
+            withContext(Dispatchers.Main) {
+                if (responseProductList.isSuccessful) {
+                    _productList.postValue(responseProductList.body()!!)
+                } else {
+                    _productError.postValue(true)
+                }
             }
         }
     }
