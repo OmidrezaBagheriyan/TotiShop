d401a3cdec83beeac51347c85b528e76b24228bc
Omidreza Bagherian <OmidCoder21@gmail.com>
2022-06-21 22:56:45 -0700

    add xml add review in project

---
+++ app/src/main/java/com/omidrezabagherian/totishop/ui/addreview/FragmentAddReview.kt
@@ -0,0 +1,7 @@
+package com.omidrezabagherian.totishop.ui.addreview
+
+import androidx.fragment.app.Fragment
+import com.omidrezabagherian.totishop.R
+
+class FragmentAddReview : Fragment(R.layout.fragment_add_review) {
+}
